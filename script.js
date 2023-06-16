f93f411ed74a22c4edb2dbafa9bfb2337f5200a2
Fabio Venturini <faventur@k0r1p8.42mulhouse.fr>
2023-06-16 16:05:40 +0200

    results

--- script.js
+++ script.js
@@ -31,7 +31,7 @@ opEl = document.querySelectorAll(".op")
 opEl.forEach(item => {
 	item.addEventListener("click", function() {
 		let isNum = outputEl.innerText
-		if (Number(isNum) !== 'Nan')
+		if (!isOperand(isNum))
 		{
 			store.push(isNum)
 			// recordOp()
@@ -43,40 +43,49 @@ opEl.forEach(item => {
 	})
 })
 
-function recordOp() {
-	if (store.length === 1) {
-		if (Number(store[0]) === 'Nan') {
-			store.pop()
-			reset()
-		}
-	} else if (store.length === 2) {
-		if (Number(store[1]) !== 'Nan') {
-			store.pop()
-			reset()
-		}
-	} else if (store.length === 3) {
-		if (Number(store[2]) === 'Nan') {
-			store.pop()
-			reset()
-		} else {
-			let str = `${Number(store[0])} ${store[1]} ${Number(store[2])}`
-			let result = eval(str)
-			store = []
-			console.log(result)
-		}
-	}
-}
-
-function equals() {
-	if (store.length === 3)
-	{
-		recordOp()
-	}
-}
+// function recordOp() {
+// 	if (store.length === 1) {
+// 		if (Number(store[0]) === 'Nan') {
+// 			store.pop()
+// 			reset()
+// 		}
+// 	} else if (store.length === 2) {
+// 		if (Number(store[1]) !== 'Nan') {
+// 			store.pop()
+// 			reset()
+// 		}
+// 	} else if (store.length === 3) {
+// 		if (Number(store[2]) === 'Nan') {
+// 			store.pop()
+// 			reset()
+// 		} else {
+// 			let str = `${Number(store[0])} ${store[1]} ${Number(store[2])}`
+// 			let result = eval(str)
+// 			store = []
+// 			console.log(result)
+// 		}
+// 	}
+// }
 
 let equalEl = document.getElementById("equal")
-equalEl.addEventListener("click", equals)
+equalEl.addEventListener("click", function() {
+	let isNum = outputEl.innerText
+	if (!isOperand(isNum))
+		store.push(isNum)
+	console.log(store)
+
+	let str = `${Number(store[0])} ${store[1]} ${Number(store[2])}`
+	let result = eval(str)
+	outputEl.innerText = result
+	store = []
+})
 
 function reset() {
 	outputEl.innerText = ""
 }
+
+let resetEl = document.getElementById("all-clear")
+resetEl.addEventListener("click", function() {
+	reset()
+	store = []
+})
